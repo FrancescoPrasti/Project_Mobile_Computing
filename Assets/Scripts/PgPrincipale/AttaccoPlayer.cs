9e175822c9f26d8ba6284f51afbe429a94be4ed7
MarDulisse <marma2212@gmail.com>
2024-01-09 09:33:41 +0100

    meleeAttack
    -Implementazione attacco con spada con relativo danno al nemico

--- Assets/Scripts/PgPrincipale/AttaccoPlayer.cs
+++ Assets/Scripts/PgPrincipale/AttaccoPlayer.cs
@@ -8,17 +8,45 @@ public class AttaccoPlayer : MonoBehaviour
     PlayerControls controls;
     public Animator animator;
 
+    public Transform attaccoCheck;
+    public LayerMask attaccoLayer;
+    public float attackRange;
+
+    bool isAttacking = false;
+
     void Awake()
     {
         controls = new PlayerControls();
         controls.Enable();
 
-        controls.Terreno.Attacco.performed += ctx => Attacco();
+        controls.Terreno.Attacco.performed += ctx =>
+        {
+            animator.SetTrigger("attacco");
+        };
     }
 
-    void Attacco()
+    private void meleeAttack()
     {
-        animator.SetTrigger("attacco");
+
+        if (isAttacking)
+        {
+            Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attaccoCheck.position, attackRange, attaccoLayer);
+            for (int i = 0; i < enemiesInRange.Length; i++)
+            {
+                enemiesInRange[i].GetComponent<Scheletro>().TakeDamage(25);
+            }
+        }
+
+    }
+
+    public void inizioAttacco()
+    {
+        isAttacking = true;
+    }
+
+    public void fineAttacco()
+    {
+        isAttacking = false;
     }
 
 }
